fc945fc81129f9640123e8786ac9830db0ef40e2
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-08 19:03:51 +0530

    Create LC:2058.cpp

---
+++ LC:2058.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> ans = {-1,-1};
+        if(head==NULL || head->next == NULL || head->next->next == NULL) return ans;
+
+        vector<int> criticalPoints(0);
+
+        ListNode* prev = head;
+        ListNode* curr = prev->next;
+        ListNode* forward = curr->next;
+
+        int position = 1;
+        while(forward!=NULL && curr!=NULL){
+            if((curr->val > prev->val && curr->val > forward->val) || (curr->val < prev->val && curr->val < forward->val)){
+                position++;
+                criticalPoints.push_back(position);
+                
+            }
+            else{
+                position++;
+            }
+            prev = curr;
+            curr=forward;
+            forward = forward->next;
+        }
+        if(criticalPoints.size() < 2) return ans;
+        cout<<criticalPoints.size()<<endl;
+
+        int minDiff = INT_MAX;
+        int maxDiff = INT_MIN;
+        for(int i = 0; i<criticalPoints.size(); i++){
+            for(int j = i+1; j<criticalPoints.size(); j++){
+                int diff = abs(criticalPoints[i]-criticalPoints[j]);
+                minDiff = min(minDiff, diff);
+                maxDiff = max(maxDiff, diff);  
+            }
+        }
+
+        ans[0]=minDiff;
+        ans[1]=maxDiff;
+        return ans;
+    }
+};

cc626ff51c871e7bf9e9b7efcf2d613693613b90
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-17 20:20:29 +0530

    Update Sliding-window_maximum.cpp

--- Sliding-window_maximum.cpp
+++ Sliding-window_maximum.cpp
@@ -1,49 +1,78 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        deque<pair<int, int>> q;
+        deque<int> q;
         vector<int> ans;
-        int maxi = INT_MIN;
-        int index = -1;
-        //q.push_back({INT_MIN, -1});
+
         for(int i = 0; i<k; i++){
-            if(arr[i]>maxi){
-                maxi = arr[i];
-                index = i;
+            while(!q.empty() && arr[i]>arr[q.back()]){
+                q.pop_back();
             }
+            q.push_back(i);
         }
-        q.push_back({maxi, index});
-        int val = q.front().first;
-        ans.push_back(val);
-        
+
+        //store ans
+        ans.push_back(arr[q.front()]);
+
         for(int i = k; i<arr.size(); i++){
-            ///store ans;
-        
-            
-                int val = q.front().first;
-                ans.push_back(val);
-            
-            //removal
-            if(!q.empty() && i-q.front().second >= k){
+            //remove
+            if(!q.empty() && i - q.front()>=k){
                 q.pop_front();
             }
-            //add
-            if(q.empty()){
-                q.push_back({arr[i],i});
+            //addition
+            while(!q.empty() && arr[q.back()] < arr[i]){
+                q.pop_back();
             }
-            else if(arr[i] > q.front().first){
-                q.pop_front();
-                q.push_back({arr[i],i});
-            }
+            q.push_back(i);
+            //store
+            ans.push_back(arr[q.front()]);
         }
-        
-        if(q.empty()){
-            ans.push_back(0);
-        }
-        else{
-            ans.push_back(q.front().first);
-        }
-        
         return ans;
+
+
+
+
+    //     int maxi = INT_MIN;
+    //     int index = -1;
+    //     //q.push_back({INT_MIN, -1});
+    //     for(int i = 0; i<k; i++){
+    //         if(arr[i]>maxi){
+    //             maxi = arr[i];
+    //             index = i;
+    //         }
+    //     }
+    //     q.push_back({maxi, index});
+    //     int val = q.front().first;
+    //     ans.push_back(val);
+        
+    //     for(int i = k; i<arr.size(); i++){
+    //         ///store ans;
+        
+            
+    //             int val = q.front().first;
+    //             ans.push_back(val);
+            
+    //         //removal
+    //         if(!q.empty() && i-q.front().second >= k){
+    //             q.pop_front();
+    //         }
+    //         //add
+    //         if(q.empty()){
+    //             q.push_back({arr[i],i});
+    //         }
+    //         else if(arr[i] > q.front().first){
+    //             q.pop_front();
+    //             q.push_back({arr[i],i});
+    //         }
+    //     }
+        
+    //     if(q.empty()){
+    //         ans.push_back(0);
+    //     }
+    //     else{
+    //         ans.push_back(q.front().first);
+    //     }
+        
+    //     return ans;
     }
 };

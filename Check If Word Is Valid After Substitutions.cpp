8ba3455393de0fd8ea3be960569b3c39298b3b18
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-21 08:22:31 +0530

    Create Check If Word Is Valid After Substitutions.cpp

---
+++ Check If Word Is Valid After Substitutions.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isValid(string s) {
+//beats 5%        
+        // if(s.size() == 0) return true;
+        // int found = s.find("abc") ;
+        // while(s.find("abc") != string::npos){
+        //     string left = s.substr(0, found);
+        //     string right = s.substr(found+3, s.size());
+        //     return isValid(left+right);
+        // }
+        // return false;
+    }
+};

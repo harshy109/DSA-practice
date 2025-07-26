ca389612acabdad377d86bd2db4430549f373ea2
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-26 23:27:42 +0530

    Update Path_sum.cpp

--- Path_sum.cpp
+++ Path_sum.cpp
@@ -36,3 +36,41 @@ public:
         return ans;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkSum(TreeNode* root, int targetSum, int sum){
+        if(root == NULL){
+            return false;
+        }
+        sum+=root->val;
+        if(root->left == NULL  && root->right == NULL){
+            if(sum == targetSum){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        bool left = checkSum(root->left, targetSum, sum);
+        bool right = checkSum(root->right, targetSum, sum);
+        return left || right;
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum = 0;
+        bool ans = checkSum(root, targetSum , sum);
+        return ans;
+    }
+};

cfba5d81a24be7ad5da486775759128572c802e4
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-29 12:23:58 +0530

    Update Count_complete_tree_node.cpp

--- Count_complete_tree_node.cpp
+++ Count_complete_tree_node.cpp
@@ -11,27 +11,60 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
+    int findLeftHeight(TreeNode* root){
+        int leftHeight = 0;
+        while(root!=NULL){
+            leftHeight++;
+            root = root->left;
+        }
+        return leftHeight;
+    }
+    int findRightHeight(TreeNode* root){
+        int rightHeight = 0;
+        while(root!=NULL){
+            rightHeight++;
+            root = root->right;
+        }
+        return rightHeight;
+    }
+    int countNodes(TreeNode* &root) {
+//O(logn * logn)        
         if(root==NULL){
             return 0;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        int count = 0;
-        while(!q.empty()){
-            TreeNode* front = q.front();
-            q.pop();
-
-            count++;
-
-            if(front->left){
-                q.push(front->left);
-            }
-            if(front->right){
-                q.push(front->right);
-            }
-
+        int leftHeight = findLeftHeight(root);
+        int rightHeight = findRightHeight(root);
+        
+        if(leftHeight == rightHeight){
+            int count = (1<<leftHeight)-1;
+            return count;
         }
-        return count;
+        else{
+            return 1 + countNodes(root->left) + countNodes(root->right);
+        }
+
+
+//O(n)        
+        // if(root==NULL){
+        //     return 0;
+        // }
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // int count = 0;
+        // while(!q.empty()){
+        //     TreeNode* front = q.front();
+        //     q.pop();
+
+        //     count++;
+
+        //     if(front->left){
+        //         q.push(front->left);
+        //     }
+        //     if(front->right){
+        //         q.push(front->right);
+        //     }
+
+        // }
+        // return count;
     }
 };

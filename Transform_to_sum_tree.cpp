abc197d739b951b892125de27ec9c37bf3aead32
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-30 19:39:22 +0530

    Update Transform_to_sum_tree.cpp

--- Transform_to_sum_tree.cpp
+++ Transform_to_sum_tree.cpp
@@ -10,7 +10,8 @@ class Solution {
 
     // Convert a given tree to a tree where every node contains sum of values of
     // nodes in left and right subtrees in the original tree
-    int solve(Node* node,int &sum, queue<int> &q){
+    //used queue to store sum
+    int solve(Node* &node,int &sum, queue<int> &q){
         if(node == NULL){
             return 0;
         }
@@ -18,15 +19,40 @@ class Solution {
         //its a leaf node
         if(node->left == NULL && node->right == NULL){
             q.push(0);
-            return node->data;
+            int val = node->data;
+            node->data = 0;
+            return val;
         }
         
         int left = solve(node->left,sum, q); //8
         int right = solve(node->right,sum,q); //-4
         sum = left + right; //0+8-4 = 4
         q.push(sum); //4
-        return sum + node->data; 
+        int val =  node->data;
+        node->data = left+right;
+        return sum + val; 
     }
+    //new approach without queue (additional data structure)
+    int solve1(Node* &node,int &sum){
+        if(node == NULL){
+            return 0;
+        }
+        
+        //its a leaf node
+        if(node->left == NULL && node->right == NULL){
+            int val = node->data;
+            node->data = 0;
+            return val;
+        }
+        
+        int left = solve1(node->left,sum); //8
+        int right = solve1(node->right,sum); //-4
+        sum = left + right; //8-4 = 4
+        int val =  node->data;
+        node->data = left+right;
+        return sum + val; 
+    }
+    
     void postOrder(Node* &node, queue<int> &q){
         if(node==NULL){
             return;
@@ -37,7 +63,7 @@ class Solution {
             q.pop();
             return;
         }
-        
+     
         postOrder(node->left,q);
         postOrder(node->right,q);
         node->data = q.front();
@@ -47,9 +73,11 @@ class Solution {
     void toSumTree(Node *node) {
         queue<int> q;
         int sum = 0;
-        solve(node,sum, q);
+        //store sum of subtrees in queue
+        // solve(node,sum, q);
+        solve1(node,sum);
         
         //postorder traversal
-        postOrder(node, q);
+        // postOrder(node, q);
     }
 };

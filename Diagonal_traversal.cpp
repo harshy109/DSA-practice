68735cbde1e6714c8425e259761f34cefeb50dbb
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-07-31 21:24:43 +0530

    Update Diagonal_traversal.cpp

--- Diagonal_traversal.cpp
+++ Diagonal_traversal.cpp
@@ -7,54 +7,35 @@ struct Node
 
 class Solution {
   public:
-    void solve(Node* &root, queue<int> &q, queue<int> &leftQ, vector<int> &ans){
-        if(root == NULL){
-            while(!q.empty()){
-                ans.push_back(q.front());
-                q.pop();
-            }
-        }
-        
-        
-    }
     vector<int> diagonal(Node *root) {
+        vector<int> ans;
+        if(root==NULL) return ans;
         queue<Node*> q;
         queue<Node*> leftQ;
-        vector<int> ans;
-        solve(root, q, leftQ, ans);
+        
         
         q.push(root);
-        int level = 0;
         
         while(!q.empty()){
             Node* front = q.front();
-            ans.push_back(q.front()->data);
             q.pop();
             if(front == NULL){
-                // while(!q.empty()){
-                //     ans.push_back(q.front()->data);
-                //     q.pop();
-                // }
-                if(level != 0){
-                    while(!leftQ.empty()){
-                        ans.push_back(leftQ.front()->data);
-                        leftQ.pop();
-                    }
+                if(!leftQ.empty()){
+                    q.push(leftQ.front());
+                    leftQ.pop();
                 }
-                q.pop();
-                q.push(ans.front()->left);
-                level++;
             }
             else{
-                if(node->right){
-                    q.push(node->right);
+                ans.push_back(front->data);
+                if(front->right){
+                    q.push(front->right);
                 }
                 else{
                     q.push(NULL);
                 }
                 
-                if(node->left){
-                    leftQ.push(node->left);
+                if(front->left){
+                    leftQ.push(front->left);
                 }
             }
         }

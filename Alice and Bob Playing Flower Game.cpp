02768b0e4a9debfd85a6427962bdab0165a85523
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-08-29 22:08:23 +0530

    Update Alice and Bob Playing Flower Game.cpp

--- Alice and Bob Playing Flower Game.cpp
+++ Alice and Bob Playing Flower Game.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        //vector<pair<int,int>> v;
+
+        //O(n2)
+        //long long count = 0;
         
-        long long count = 0;
-        
-        for(int i=1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                //v.push_back(make_pair(i,j));
-                int sum = i+j;
-                if(sum%2 != 0){
-                    count++;
-            }
-            }
-        }
-        // long long count = 0;
-        // for(auto pt : v){
-        //     int sum = pt.first + pt.second;
-        //     if(sum%2 != 0){
-        //         count++;
+        // for(int i=1; i<=n; i++){
+        //     for(int j = 1; j<=m; j++){
+        //         //v.push_back(make_pair(i,j));
+        //         int sum = i+j;
+        //         if(sum%2 != 0){
+        //             count++;
+        //         }
         //     }
         // }
-        return count;
+
+        //Optimized approach with TC = O(n)
+        long long oddX = 0;
+        long long evenX = 0;
+        long long oddY = 0;
+        long long evenY = 0;
+
+        for(int i = 0; i<n; i++){
+            if(i%2 != 0){
+                oddX++;
+            }
+            else{
+                evenX++;
+            }
+        }
+        for(int i = 0; i<m; i++){
+            if(i%2 != 0){
+                oddY++;
+            }
+            else{
+                evenY++;
+            }
+        }
+
+        long long combA = oddX * evenY;
+        long long combB = evenX * oddY;
+
+        return combA+combB;
     }
 };

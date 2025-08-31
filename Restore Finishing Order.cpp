e462d3af9a63aca3f43096db8a89c48f0e0b58a4
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-08-31 12:41:11 +0530

    Update Restore Finishing Order.cpp

--- Restore Finishing Order.cpp
+++ Restore Finishing Order.cpp
@@ -13,6 +13,20 @@ public:
         // }
         // return ans;
 
+        //using set - O(n)
+        unordered_set<int> s;
+        vector<int> ans;
         
+        for(int i = 0; i<friends.size(); i++){
+            s.insert(friends[i]);
+        }
+
+        for(int i=0; i<order.size(); i++){
+            if(s.find(order[i])!=s.end()){
+                ans.push_back(order[i]);
+            }
+        }
+
+        return ans;
     }
 };

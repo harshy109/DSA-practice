4ca0a53884f2640004f7e19383423f659b0e35ae
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-08-31 12:38:43 +0530

    Create Restore Finishing Order.cpp

---
+++ Restore Finishing Order.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        //Bruteforce = O(n*f)
+        // vector<int> ans;
+        
+        // for(int i = 0; i<order.size(); i++){
+        //     for(int j = 0; j<friends.size(); j++ ){
+        //         if(order[i] == friends[j]){
+        //             ans.push_back(order[i]);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        
+    }
+};

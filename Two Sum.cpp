a04e64a10bb675e3a9951d1fe8322230e6caf3ae
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-01 00:04:01 +0530

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -25,32 +25,53 @@ public:
         // }
         // return v;
 
-        unordered_map<int, pair<int, int>> mp;
+        //using map - 30-08-25
+        // unordered_map<int, pair<int, int>> mp;
 
+        // for(int i = 0; i<arr.size(); i++){
+        //     for(int j = i+1; j<arr.size(); j++){
+        //         int sum = arr[i]+arr[j];
+        //         mp[sum] = {i,j};
+        //     }
+        // }
+
+        // vector<int> ans;
+        // for(auto it : mp){
+        //     if(it.first == target){
+        //         ans.push_back(it.second.first);
+        //         ans.push_back(it.second.second);
+        //         break;
+        //     }
+        // }
+
+        // return ans;
+
+
+        //31-8-25
+        unordered_map<int, int> mp;
         for(int i = 0; i<arr.size(); i++){
-            for(int j = i+1; j<arr.size(); j++){
-                int sum = arr[i]+arr[j];
-                mp[sum] = {i,j};
-            }
+            //mp[arr[i]] = i;
+            mp[i] = arr[i];
         }
 
         vector<int> ans;
-        for(auto it : mp){
-            if(it.first == target){
-                ans.push_back(it.second.first);
-                ans.push_back(it.second.second);
-                break;
+        bool found = false;
+
+        for(int i = 0; i<arr.size(); i++){
+            int currElement = arr[i];
+            int valueToFind = target - currElement;
+    
+            for(auto it : mp){
+                if(it.first != i && it.second == valueToFind){
+                    ans.push_back(i);
+                    ans.push_back(it.first);
+                    found = true;
+                    break;
+                }
             }
+            if(found) break;
         }
-
         return ans;
-
-
-
-
-
-
-
 
         // int n=arr.size();
         // int i,j;

ce410e2a0f10e03213fd095e93f8136991a1d8f1
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-05 19:11:37 +0530

    Update Array Subset.cpp

--- Array Subset.cpp
+++ Array Subset.cpp
@@ -3,25 +3,47 @@ class Solution {
   public:
     // Function to check if b is a subset of a
     bool isSubset(vector<int> &a, vector<int> &b) {
-        unordered_map<int,int> bmp;
-        unordered_map<int,int> amp;
+        // unordered_map<int,int> bmp;
+        // unordered_map<int,int> amp;
         
         
-        for(int i=0;i<b.size(); i++){
-            bmp[b[i]]++;
+        // for(int i=0;i<b.size(); i++){
+        //     bmp[b[i]]++;
+        // }
+        
+        // for(int i=0; i<a.size(); i++){
+        //     if(bmp.find(a[i]) != bmp.end() && bmp[a[i]]>0){
+        //         bmp[a[i]]--;
+        //     }
+        // }
+        
+        // for(auto it : bmp){
+        //     if(it.second != 0){
+        //         return false;
+        //     }
+        // }
+        
+        
+        unordered_map<int,int> map;
+        
+        for(int i =0; i<a.size(); i++){
+            map[a[i]]++;
         }
         
-        for(int i=0; i<a.size(); i++){
-            if(bmp.find(a[i]) != bmp.end() && bmp[a[i]]>0){
-                bmp[a[i]]--;
-            }
-        }
-        
-        for(auto it : bmp){
-            if(it.second != 0){
+        for(int i =0; i<b.size(); i++){
+            if(map.find(b[i]) == map.end()){
                 return false;
             }
+            else{
+                if(map[b[i]] > 0){
+                    map[b[i]]--;
+                }
+                else{
+                    return false;
+                }
+            }
         }
+        
         
         return true;
     }

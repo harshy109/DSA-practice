1a79ad126f526ced565d0adfc8fc2e9377d262df
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-09 01:35:42 +0530

    Update Pairs of equals Sum.cpp

--- Pairs of equals Sum.cpp
+++ Pairs of equals Sum.cpp
@@ -7,14 +7,18 @@ class Solution {
             for(int j = i+1; j<arr.size(); j++){
                 int sum = arr[i] + arr[j];
                 map[sum]++;
+                //optimizaton applied
+                if(map[sum] == 2){
+                    return true;
+                }
             }
         }
-        
-        for(auto it : map){
-            if(it.second == 2){
-                return true;
-            }
-        }
+        //TLE
+        // for(auto it : map){
+        //     if(it.second == 2){
+        //         return true;
+        //     }
+        // }
         return false;
     }
 };

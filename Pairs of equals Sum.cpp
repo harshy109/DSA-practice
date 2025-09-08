4b15dcc389248745360fdcff23892f2e22f95490
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-09 01:28:29 +0530

    Create Pairs of equals Sum.cpp

---
+++ Pairs of equals Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool findPairs(vector<int>& arr) {
+        unordered_map<int, int> map;
+        
+        for(int i = 0; i<arr.size(); i++){
+            for(int j = i+1; j<arr.size(); j++){
+                int sum = arr[i] + arr[j];
+                map[sum]++;
+            }
+        }
+        
+        for(auto it : map){
+            if(it.second == 2){
+                return true;
+            }
+        }
+        return false;
+    }
+};

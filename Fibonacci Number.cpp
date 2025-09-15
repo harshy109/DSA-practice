815487d10103e495aeeb3cf863e238d046e50f73
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-15 14:47:57 +0530

    Update Fibonacci Number.cpp

--- Fibonacci Number.cpp
+++ Fibonacci Number.cpp
@@ -30,12 +30,63 @@ public:
         return dp[n];
     }
 
-    int fib(int n) {
-
-        //step1 : create an array, ans pass in function
+    int solveUsingTabulation(int n){
+        //step1 : create a dp array
         vector<int> dp(n+1, -1);
-        int ans = solveUsingDp(n, dp);
-        return ans;
+
+        //analyse base cases and update dp array
+        dp[0] = 0;
+        if(n == 0){
+            return 0;
+        }
+        dp[1] = 1;
+
+        //check parameter, reverse it and run a loop
+        //recursion : n -> 0
+        //reverse it : 0 -> n
+        for(int i = 2; i<=n; i++){
+            //copy paste
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+    int solveUsingTabSpaceOptimized(int n){
+        //step1 : create a dp array
+        //vector<int> dp(n+1, -1);
+
+        //analyse base cases and update dp array
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        int prev = 0;
+        int curr = 1;
+
+        for(int i = 2; i<=n; i++){
+            int ans = prev + curr;
+            prev = curr;
+            curr = ans;
+        }
+
+        return curr;
+    }
+
+    int fib(int n) {
+        //top down approach
+        //step1 : create an array, ans pass in function
+        // vector<int> dp(n+1, -1);
+        // int ans = solveUsingDp(n, dp);
+        // return ans;
+
+        //bottom up approach
+        // return solveUsingTabulation(n);
+
+        //space optmized
+        return solveUsingTabSpaceOptimized(n);
 
 
         //LOOP

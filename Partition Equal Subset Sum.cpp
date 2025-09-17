c24891cbe6641f74d674f98b9de5e7d537f2b9b6
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-17 23:34:43 +0530

    Update Partition Equal Subset Sum.cpp

--- Partition Equal Subset Sum.cpp
+++ Partition Equal Subset Sum.cpp
@@ -40,7 +40,6 @@ public:
         int n = nums.size();
         vector<vector<int>> dp(n+2, vector<int> (halfSum+100, 0));
 
-
         for(int row = 0; row<=n; row++){
             dp[row][halfSum] = 1;
         }
@@ -58,6 +57,31 @@ public:
         }
         return dp[0][0];
     }
+    bool solveTabSO(vector<int>& nums, int halfSum){
+        int n = nums.size();
+
+        vector<int> next(halfSum+1, 0);
+        vector<int> curr(halfSum+1, 0);
+
+        next[halfSum] = 1;
+
+        //range
+        //i = 0->n
+        //total = 0-> halfsum / total
+        for(int i = n-1; i>=0; i--){
+            for(int t = halfSum; t>=0; t--){
+                bool include = 0;
+                if(t+nums[i] <= halfSum){
+                    include = next[t + nums[i]]; 
+                }
+                bool exclude = next[t];
+
+                curr[t] = include || exclude;
+            }
+            next = curr;
+        }
+        return curr[0];
+    }    
     bool canPartition(vector<int>& nums) {
         int sum = 0 ; 
         for(int i = 0; i<nums.size(); i++){
@@ -74,6 +98,6 @@ public:
         // vector<vector<int>> dp(n+1, vector<int> (sum+1, -1));
         // return solveMem(nums,halfSum, 0, total,dp);
 
-        return solveTab(nums, halfSum);
+        return solveTabSO(nums, halfSum);
     }
 };

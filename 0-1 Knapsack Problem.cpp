560c1cf615339d957441282319455ab0c8ff763d
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-17 19:35:38 +0530

    Update 0-1 Knapsack Problem.cpp

--- 0-1 Knapsack Problem.cpp
+++ 0-1 Knapsack Problem.cpp
@@ -44,6 +44,70 @@ class Solution {
         dp[capacity][index] = max(inc, exc);
         return dp[capacity][index];
     }
+    int solveWithTab(int capacity, vector<int> &val, vector<int> &wt){
+        int n = val.size();
+        vector<vector<int>> dp(capacity+1, vector<int> (n+1, 0));
+        
+        
+        //ananlyse base case
+        for(int w = wt[0]; w<=capacity; w++){
+            dp[w][0] = val[0]; //important to initialise to work, on some intial value
+        }
+        
+        //range find
+        //capacity : w -> 0
+        //index  : n-1 -> 0
+        
+        for(int weight = 0; weight<=capacity; weight++){
+            for(int index = 1; index<=n-1; index++){
+                //ek case me
+                //include exclude patterm
+                int inc = 0;
+                if(wt[index] <= weight){
+                    inc = val[index] + dp[weight-wt[index]][index-1];
+                }
+                int exc = dp[weight][index-1];
+                
+                //ans store and return
+                dp[weight][index] = max(inc, exc);
+            }
+        }
+        
+        return dp[capacity][n-1];
+    }
+    int solveWithTabSO(int capacity, vector<int> &val, vector<int> &wt){
+        int n = val.size();
+        
+        vector<int> prev(capacity+1, 0);
+        vector<int> curr(capacity+1, 0);
+        
+        //ananlyse base case
+        for(int w = wt[0]; w<=capacity; w++){
+            //since hum 
+            prev[w] = val[0]; //important to initialise to work, on some intial value
+        }
+        
+        //range find
+        //capacity : w -> 0
+        //index  : n-1 -> 0
+        
+        for(int weight = 0; weight<=capacity; weight++){
+            for(int index = 1; index<=n-1; index++){
+                //ek case me
+                //include exclude patterm
+                int inc = 0;
+                if(wt[index] <= weight){
+                    inc = val[index] + dp[weight-wt[index]][index-1];
+                }
+                int exc = dp[weight][index-1];
+                
+                //ans store and return
+                dp[weight][index] = max(inc, exc);
+            }
+        }
+        
+        return dp[capacity][n-1];
+    }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
         int n = val.size()-1;
@@ -52,9 +116,10 @@ class Solution {
         
         //top dowm appproach
         //2d array since capacity and i both are changing
-        vector<vector<int>> dp(W+1, vector<int> (n+1, -1));
-        return solveWithMem(val, wt, W, n, dp);
+        // vector<vector<int>> dp(W+1, vector<int> (n+1, -1));
+        // return solveWithMem(val, wt, W, n, dp);
         
+        return solveWithTab(W,val, wt);
         
     }
 };

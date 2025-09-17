0f9820b631b0a4b7c3c8cdfb638c1aecc8de11eb
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-17 22:08:57 +0530

    Implement solveTabulation for fence painting problem
    Added tabulation method for counting ways to paint a fence.

--- Painting fence.cpp
+++ Painting fence.cpp
@@ -31,10 +31,25 @@ class Solution {
         dp[n] = ((k-1) * (solveMem(n-1, k, dp)%mod + solveMem(n-2, k, dp)%mod)%mod)%mod;
         return dp[n];
     }
+    int solveTabulation(int n, int k){
+        vector<int> dp(n+1, 0);
+        
+        dp[1] = k;
+        dp[2] = (k*k)%mod;
+        
+        //check range
+        ///n : n->1
+        for(int i = 3; i<=n; i++){
+            dp[i] = ((k-1) * (dp[i-1]%mod + dp[i-2]%mod)%mod)%mod;
+        }
+        return dp[n];
+    }
     int countWays(int n, int k) {
         //return solveRec(n, k);
         
-        vector<int> dp(n+1, -1);
-        return solveMem(n, k ,dp);
+        // vector<int> dp(n+1, -1);
+        // return solveMem(n, k ,dp);
+        
+        return solveTabulation(n,k);
     }
 };

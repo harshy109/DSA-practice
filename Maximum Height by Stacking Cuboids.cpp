b70c069d4b6271bffea487b793bced4ba5f4eb6d
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-18 19:38:35 +0530

    Update Maximum Height by Stacking Cuboids.cpp

--- Maximum Height by Stacking Cuboids.cpp
+++ Maximum Height by Stacking Cuboids.cpp
@@ -58,6 +58,27 @@ public:
         }
         return dp[0][0];
     }
+    int solveTabSO(vector<vector<int>>& cuboids){
+        int n = cuboids.size();
+        vector<int> prevRow(n+1, 0);
+        vector<int> currRow(n+1, 0);
+
+        //range
+        //curr : 0 -> n
+        //prev : -1 -> n
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                int inc = 0;
+                if(prev == -1 || isSafeToPlace(cuboids[prev], cuboids[curr])){
+                    inc = cuboids[curr][2] + prevRow[curr+1];
+                }
+                int exc = 0 + prevRow[prev+1];
+                currRow[prev+1] = max(inc, exc);
+            }
+            prevRow = currRow;
+        }
+        return prevRow[0];
+    }    
     int maxHeight(vector<vector<int>>& cuboids) {
         //sorting level 1 : individual sorting
         for(auto &cuboid : cuboids){
@@ -75,7 +96,9 @@ public:
         // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
         //return solveMem(cuboids, curr, prev, dp);
 
-        return solveTab(cuboids);
+        //return solveTabSO(cuboids);
+
+        return solveTabSO(cuboids);
         
     }
 };

7b3e97f812149b8d4a741f387645c1e36c6c7eca
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-18 17:06:43 +0530

    Update Longest Common Subsequence.cpp

--- Longest Common Subsequence.cpp
+++ Longest Common Subsequence.cpp
@@ -42,11 +42,33 @@ public:
         dp[i][j] = ans;
         return  dp[i][j];
     }
+    int solveTab(string &text1, string &text2){
+        //step 1
+        vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, 0));
+        //step 2
+
+        //range i: 0->n , j:0->n
+        for(int i = text1.length()-1; i>=0; i--){
+            for(int j = text2.length()-1; j>=0; j--){
+                int ans = 0;
+                if(text1[i] == text2[j]){
+                    ans = 1 + dp[i+1][j+1];
+                }
+                else{
+                    ans = 0 + max(dp[i+1][j], dp[i][j+1]);
+                }
+                dp[i][j] = ans;                
+            }
+        }
+        return dp[0][0];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         //return solveRec(text1,text2, 0,0);
 
         //2D dp
-        vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, -1));
-        return solveMem(text1, text2, 0, 0, dp);
+        // vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, -1));
+        // return solveMem(text1, text2, 0, 0, dp);
+
+        return solveTab(text1, text2);
     }
 };

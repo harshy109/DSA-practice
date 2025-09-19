b24ac9dbdf4ecf1b3e03292c4720af856b3f90a8
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-19 19:22:29 +0530

    Update Minimum cost tree from leaf node.cpp

--- Minimum cost tree from leaf node.cpp
+++ Minimum cost tree from leaf node.cpp
@@ -34,6 +34,32 @@ public:
         dp[s][e] = ans;
         return dp[s][e];
     }
+    int solveTab(vector<int>& arr,map<pair<int, int>, int> &maxi){
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1, 0));
+
+        //range : 
+        //s : 0-> n
+        //e : n-1 -> 0
+        for(int s = n; s>=0; s--){
+            for(int e = 0; e<=n-1; e++){
+                if(s>=e){
+                    continue;
+                }
+                else{
+                    int ans = INT_MAX;
+                    for(int i = s ; i<e; i++){
+                        int leftRangeKaMax = maxi[{s,i}];
+                        int rightRangeKaMax = maxi[{i+1, e}];
+                        int nonLeafValue = leftRangeKaMax*rightRangeKaMax;
+                        ans = min(ans, nonLeafValue + dp[s][i] + dp[i+1][e]);
+                    }
+                    dp[s][e] = ans;
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
     int mctFromLeafValues(vector<int>& arr) {
         int n = arr.size();
         map<pair<int, int>, int> maxi;
@@ -53,7 +79,9 @@ public:
         int e = n-1;
         // return solveRec(arr,maxi, s, e);
 
-        vector<vector<int>> dp(n+1,vector<int>(n+1, -1));
-        return solveMem(arr, maxi, s, e, dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1, -1));
+        // return solveMem(arr, maxi, s, e, dp);
+
+        return solveTab(arr, maxi);
     }
 };

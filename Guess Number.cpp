48b2da5b37f188411499dd84c5aa15b56a8b3f6b
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-19 23:09:45 +0530

    Update Guess Number.cpp

--- Guess Number.cpp
+++ Guess Number.cpp
@@ -13,10 +13,10 @@ public:
         int s = 1, e = n;
         while(s<=e){
             int mid = s+(e-s)/2;
-            if(guess(mid)==0){
+            if(guess(mid) == 0){
                 return mid;
             }
-            else if(guess(mid)==1){
+            else if(guess(mid)==-1){
                 e=mid-1;
             }
             else{

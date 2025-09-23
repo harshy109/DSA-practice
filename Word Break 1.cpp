c21b02f3dead14f6ac9f33078e0dc21961a6558c
Harshita Salvi <141760030+harshy109@users.noreply.github.com>
2025-09-23 23:43:16 +0530

    Refactor word break solution logic
    Refactor the solve function to check for word presence and recursively call itself with updated strings.

--- Word Break 1.cpp
+++ Word Break 1.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+    bool solve(string& s, vector<string>& wordDict) {
+        // base case
+        if(s.length() == 0) {
+            return true;
+        }
+        for(string word : wordDict) {
+            if (s.size() == 0) {
+                return true;
+            }
+            //auto it = s.find(word) ? s.find(word) : string ::npos;
+            if (s.find(word) != string::npos) {
+                string s1 = s.substr(0, word.length());
+                string s2 = s.substr(word.length());
+                //cout<<s1<<" and "<<s2<<endl;
+                string newStr = s1 + s2;
+                solve(newStr, wordDict);
+            }
+        }
+        return false;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
+        return solve(s, wordDict);
     }
 };
